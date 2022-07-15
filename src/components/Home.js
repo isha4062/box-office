60b424c91fd1085898d7dbb06e3e0b3726334e8b
Isha <isha.20hcs4125@ddu.du.ac.in>
2022-07-15 14:43:59 +0530

    created and styled cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
 /* eslint-disable no-console */
 import React , {useState} from 'react';
+import ActorGrid from './actor/ActorGrid';
 import Mainpage from './Mainpage';
+import ShowGrid from './show/ShowGrid';
 
 function Home() {
   const [input, setInput] = useState('');
   const [result , setResult] = useState(null);
+  const [search , setSearch] = useState('shows');
+
+  const isChecked = search === 'shows';
   const onInputChange = (ev) => {
     setInput(ev.target.value);
   }
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(res => setResult(res));
+    fetch(`https://api.tvmaze.com/search/${search}?q=${input}`).then(r => r.json()).then(res => setResult(res));
   }
   const onEnter = (ev) => {
     if(ev.keyCode === 13){
@@ -21,14 +26,27 @@ function Home() {
         return <div>No Results</div>;
     }
     if(result && result.length > 0){
-        return <div>{result.map(el => <div key={el.show.id}>{el.show.name}</div>)}</div>
+        return result[0].show? <ShowGrid result={result} /> : <ActorGrid result={result} />
     }
     return null;
+  }
+  const setSearchOption = (ev) => {
+    setSearch(ev.target.value);
   }
   return (
     <Mainpage>
           <input type="text" onChange={onInputChange} onKeyDown={onEnter} value={input} />
           <button type="submit" onClick={onSearch}>Search</button>
+          <div>
+            <label htmlFor="shows">
+              Shows
+              <input type="radio" id='shows' value='shows' onChange={setSearchOption} checked={isChecked}/>
+            </label>
+            <label htmlFor="shows">
+              Actors
+              <input type="radio" id='actors' value='people' onChange={setSearchOption} checked={!isChecked}/>
+            </label>
+          </div>
           {searchResult()}
       </Mainpage>
   );
